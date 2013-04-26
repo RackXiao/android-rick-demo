66281cb636abff9a771e51fdeed0ffbb8d0ee1fd
kongtern@gmail.com <kongtern@gmail.com@6540b9a8-685d-fb2a-bc52-5fde6eb39722>
2013-04-26 06:49:29 +0000

    完成CDP計算功能

---
+++ trunk/src/tw/rackx/layout/LayoutCalc.java
@@ -0,0 +1,150 @@
+package tw.rackx.layout;
+
+import tw.rackx.R;
+import tw.rackx.app.GV;
+import tw.rackx.util.LayoutManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class LayoutCalc {
+
+	private static int ID = 10000;
+	
+	// getCalcMenuLayout
+	public static int BtnCalcCDP = ++LayoutManager.BaseId;
+
+	// getCalcCDPLayout
+	public static int ETOPEN = ++LayoutManager.BaseId;
+	public static int ETHIGH = ++LayoutManager.BaseId;
+	public static int ETLOW = ++LayoutManager.BaseId;
+	public static int ETCLOSE = ++LayoutManager.BaseId;
+	public static int ETBtnCalcCDP = ++LayoutManager.BaseId;
+	public static int ETTVH3 = ++LayoutManager.BaseId;
+	public static int ETTVH2 = ++LayoutManager.BaseId;
+	public static int ETTVH1 = ++LayoutManager.BaseId;
+	public static int ETTVBOP = ++LayoutManager.BaseId;
+	public static int ETTVL1 = ++LayoutManager.BaseId;
+	public static int ETTVL2 = ++LayoutManager.BaseId;
+	public static int ETTVL3 = ++LayoutManager.BaseId;
+	
+	/**
+	 * 計算功能選單
+	 * @param context
+	 * @return
+	 */
+	public static View getCalcMenuLayout(Context context) {
+		RelativeLayout baseLayout = new RelativeLayout(context);
+		RelativeLayout.LayoutParams rParams = null;
+		Button btn = null;
+
+		rParams = new RelativeLayout.LayoutParams((int)(160 * GV.scale), (int)(160 * GV.scale));
+		btn = new Button(context);
+		btn.setId(BtnCalcCDP);
+		btn.setText(R.string.calc_cdp);
+		baseLayout.addView(btn, rParams);
+
+//		rParams = new RelativeLayout.LayoutParams((int)(160 * GV.scale), (int)(160 * GV.scale));
+//		rParams.setMargins((int)(160 * GV.scale), 0, 0, 0);
+//		btn = new Button(context);
+//		btn.setId(BtnDialog);
+//		btn.setText(R.string.btn_dialog);
+//		baseLayout.addView(btn, rParams);
+//
+//		rParams = new RelativeLayout.LayoutParams((int)(160 * GV.scale), (int)(160 * GV.scale));
+//		rParams.setMargins((int)(320 * GV.scale), 0, 0, 0);
+//		btn = new Button(context);
+//		btn.setId(BtnTabMenu);
+//		btn.setText(R.string.btn_show_tabmenu);
+//		baseLayout.addView(btn, rParams);
+
+		return LayoutManager.setBaseLayoutScreenResolution(baseLayout);
+	}
+
+	/**
+	 * 計算CDP功能
+	 * @param context
+	 * @return
+	 */
+	public static View getCalcCDPLayout(Context context) {
+		RelativeLayout baseLayout = new RelativeLayout(context);
+		RelativeLayout rsv = new RelativeLayout(context);
+		RelativeLayout.LayoutParams rParams = null;
+		ScrollView sv = new ScrollView(context);
+		TextView tv = null;
+		EditText et = null;
+		Button btn = null;
+
+		int [] tvLabel = new int[] {R.string.calc_cdp_open,R.string.calc_cdp_high,R.string.calc_cdp_low,R.string.calc_cdp_close};
+		int [] edID = new int[] {ETOPEN,ETHIGH,ETLOW,ETCLOSE};
+		
+		for(int i=0;i<tvLabel.length;i++){
+			// 標籤
+			rParams = new RelativeLayout.LayoutParams((int)(240 * GV.scale), (int)(60 * GV.scale));
+			rParams.setMargins(0, (int)(i * 60 * GV.scale), 0, 0);
+			tv = new TextView(context);
+			tv.setId(++ID);
+			tv.setTextSize(16 * GV.fontScale);
+			tv.setGravity(Gravity.CENTER);
+			tv.setTextColor(Color.rgb(53, 214, 214));
+			tv.setText(tvLabel[i]);
+			baseLayout.addView(tv, rParams);
+			
+			// 輸入框
+			rParams = new RelativeLayout.LayoutParams((int)(240 * GV.scale), (int)(60 * GV.scale));
+			rParams.addRule(RelativeLayout.RIGHT_OF, ID);
+			rParams.addRule(RelativeLayout.ALIGN_TOP, ID);
+			et = new EditText(context);
+			et.setId(edID[i]);
+			et.setSingleLine(true);
+			et.setTextSize(16 * GV.fontScale);
+			et.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+//			et.setTextColor(Color.BLACK);
+//			et.setHint("");
+			et.setInputType(InputType.TYPE_CLASS_NUMBER);
+			baseLayout.addView(et, rParams);
+
+		}
+
+		// 計算按鈕
+		rParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, (int)(60 * GV.scale));
+		rParams.addRule(RelativeLayout.BELOW, ID);
+		rParams.addRule(RelativeLayout.CENTER_HORIZONTAL, ID);
+		btn = new Button(context);
+		btn.setId(ETBtnCalcCDP);
+		btn.setText(R.string.btn_calc_cdp);
+		baseLayout.addView(btn, rParams);
+
+
+		rParams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		rParams.addRule(RelativeLayout.BELOW, ETBtnCalcCDP);
+		rParams.addRule(RelativeLayout.CENTER_HORIZONTAL, ETBtnCalcCDP);
+		sv.addView(rsv);
+		baseLayout.addView(sv,rParams);
+
+		edID = new int[] {ETTVH3,ETTVH2,ETTVH1,ETTVBOP,ETTVL1,ETTVL2,ETTVL3};
+
+		// 壓 BOP 撐
+		for(int i=0;i<edID.length;i++){
+			rParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, (int)(60 * GV.scale));
+			rParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+			rParams.setMargins(0, (int)(i * 60 * GV.scale), 0, 0);
+			tv = new TextView(context);
+			tv.setId(edID[i]);
+			tv.setTextSize(16 * GV.fontScale);
+			tv.setGravity(Gravity.CENTER);
+			tv.setTextColor(Color.rgb(53, 214, 214));
+			rsv.addView(tv, rParams);
+		}
+		
+		return LayoutManager.setBaseLayoutScreenResolution(baseLayout);
+	}
+}
